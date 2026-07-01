1a4ffb9477a95b3201a178fef14d2760cb6fe034
gordo240620 <manuelpacheco031105@gmail.com>
2026-06-30 23:48:08 -0600

    Carpeteo del proyecto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { Login } from './auth/login/login';
+import { Register } from './auth/register/register';
+import { Dashboard } from './dashboard/dashboard';
+import { authGuard } from './guards/auth-guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+
+  { path: 'login', component: Login },
+
+  { path: 'register', component: Register },
+
+  {
+    path: 'dashboard',
+    component: Dashboard,
+    canActivate: [authGuard]
+  },
+
+  { path: '**', redirectTo: 'login' }
+];
