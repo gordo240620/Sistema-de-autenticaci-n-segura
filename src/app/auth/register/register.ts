5abb82b9c45e6fd01d3db7d4c43f1979a22d7135
alexcantu22 <alejandritocantu5@gmail.com>
2026-07-03 09:29:55 -0700

    Registro con validaciones

--- src/app/auth/register/register.ts
+++ src/app/auth/register/register.ts
@@ -1,26 +1,62 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [FormsModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './register.html',
   styleUrl: './register.css'
 })
 export class Register {
 
-  nombre = '';
-  correo = '';
-  password = '';
-  confirmarPassword = '';
+  nombre: string = '';
+  correo: string = '';
+  password: string = '';
+  confirmarPassword: string = '';
 
-  registrar() {
+  mensajeError: string = '';
+  mensajeExito: string = '';
 
-    console.log(this.nombre);
-    console.log(this.correo);
-    console.log(this.password);
-    console.log(this.confirmarPassword);
+  registrar(): void {
+
+    this.mensajeError = '';
+    this.mensajeExito = '';
+
+    if (
+      !this.nombre ||
+      !this.correo ||
+      !this.password ||
+      !this.confirmarPassword
+    ) {
+      this.mensajeError = 'Todos los campos son obligatorios.';
+      return;
+    }
+
+    const correoValido = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (!correoValido.test(this.correo)) {
+      this.mensajeError = 'Ingrese un correo válido.';
+      return;
+    }
+
+    if (this.password.length < 8) {
+      this.mensajeError = 'La contraseña debe tener al menos 8 caracteres.';
+      return;
+    }
+
+    if (this.password !== this.confirmarPassword) {
+      this.mensajeError = 'Las contraseñas no coinciden.';
+      return;
+    }
+
+    this.mensajeExito = 'Usuario registrado correctamente.';
+
+    this.nombre = '';
+    this.correo = '';
+    this.password = '';
+    this.confirmarPassword = '';
 
   }
 
